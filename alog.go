27a9cdac85523333e654f0974c266387c08e5d69
alonedelshtein <alonedelshtein@gmail.com>
2022-07-05 16:14:30 +0300

    commit M3

--- alog.go
+++ alog.go
@@ -29,21 +29,34 @@ func New(w io.Writer) *Alog {
 		w = os.Stdout
 	}
 	return &Alog{
-		dest:    w,
-		msgCh:   make(chan string),
-		errorCh: make(chan error),
-		m:       &sync.Mutex{},
+		dest:               w,
+		msgCh:              make(chan string),
+		errorCh:            make(chan error),
+		m:                  &sync.Mutex{},
+		shutdownCh:         make(chan struct{}),
+		shutdownCompleteCh: make(chan struct{}),
 	}
 }
 
 // Start begins the message loop for the asynchronous logger. It should be initiated as a goroutine to prevent
 // the caller from being blocked.
 func (al Alog) Start() {
-	for msg := range al.msgCh {
-		go func(msg string, wg *sync.WaitGroup) {
-			al.write(msg, wg)
-		}(msg, nil)
+	wg := &sync.WaitGroup{}
+	for {
+		select {
+		case msg := <-al.msgCh:
+			wg.Add(1)
+			go func(msg string, wg *sync.WaitGroup) {
+				al.write(msg, wg)
+			}(msg, wg)
+
+		case <-al.shutdownCh:
+			wg.Wait()
+			al.shutdown()
+			goto end
+		}
 	}
+end:
 }
 
 func (al Alog) formatMessage(msg string) string {
@@ -57,10 +70,12 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 	al.m.Lock()
 	defer func() {
 		if r := recover(); r != nil {
+			wg.Done()
 			al.m.Unlock()
 		}
 	}()
 	_, err := al.dest.Write([]byte(al.formatMessage(msg)))
+	wg.Done()
 	al.m.Unlock()
 
 	if err != nil {
@@ -72,6 +87,8 @@ func (al Alog) write(msg string, wg *sync.WaitGroup) {
 }
 
 func (al Alog) shutdown() {
+	close(al.msgCh)
+	al.shutdownCompleteCh <- struct{}{}
 }
 
 // MessageChannel returns a channel that accepts messages that should be written to the log.
@@ -89,6 +106,10 @@ func (al Alog) ErrorChannel() <-chan error {
 // Stop shuts down the logger. It will wait for all pending messages to be written and then return.
 // The logger will no longer function after this method has been called.
 func (al Alog) Stop() {
+	al.shutdownCh <- struct{}{}
+	for range al.shutdownCompleteCh {
+		break
+	}
 }
 
 // Write synchronously sends the message to the log output
